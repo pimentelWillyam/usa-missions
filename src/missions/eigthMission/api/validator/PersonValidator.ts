749e0ea27e30a8e7a65c25c6cff3c90883f94f53
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-15 15:59:17 -0300

    refactor: changing errors to throw at the validator

--- src/missions/eigthMission/api/validator/PersonValidator.ts
+++ src/missions/eigthMission/api/validator/PersonValidator.ts
@@ -1,4 +1,5 @@
 import type IPersonValidator from '../interface/IPersonValidator'
+import InvalidAgeError from './errors/person/InvalidAgeError'
 import InvalidDataTypeError from './errors/person/InvalidDataTypeError'
 import InvalidEmailError from './errors/person/InvalidEmailError'
 import NameHasAnyNumberError from './errors/person/NameHasAnyNumberError'
@@ -6,39 +7,39 @@ import NameLengthBelowFourLettersError from './errors/person/NameLengthBelowFour
 
 class PersonValidator implements IPersonValidator {
   validate (name: string, email: string, age: number): void {
-    if (!this.nameIsLongEnough(name)) throw new NameLengthBelowFourLettersError()
-    if (!this.nameHasNoNumbers(name)) throw new NameHasAnyNumberError()
-    if (!this.isEmailValid(email)) throw new InvalidEmailError()
-    if (!this.isAgeValid(age)) throw new InvalidEmailError()
     if (this.valueIsNullOrUndefined('name')) throw new InvalidDataTypeError('nome')
     if (this.valueIsNullOrUndefined('email')) throw new InvalidDataTypeError('email')
     if (this.valueIsNullOrUndefined('age')) throw new InvalidDataTypeError('idade')
+    if (!this.nameIsLongEnough(name)) throw new NameLengthBelowFourLettersError()
+    if (!this.nameHasNoNumbers(name)) throw new NameHasAnyNumberError()
+    if (!this.isEmailValid(email)) throw new InvalidEmailError()
+    if (!this.isAgeValid(age)) throw new InvalidAgeError()
   }
 
   private readonly nameIsLongEnough = (name: string): boolean => {
-    if (name.length <= 3) throw new Error('O nome da pessoa precisa ter mais de três caracteres')
+    if (name.length <= 3) return false
     return true
   }
 
   private readonly nameHasNoNumbers = (name: string): boolean => {
     for (const letter of name) {
-      if (letter === '0' || letter === '1' || letter === '2' || letter === '3' || letter === '4' || letter === '5' || letter === '6' || letter === '7' || letter === '8' || letter === '9') throw new Error('O nome da pessoaa não deve conter números')
+      if (letter === '0' || letter === '1' || letter === '2' || letter === '3' || letter === '4' || letter === '5' || letter === '6' || letter === '7' || letter === '8' || letter === '9') return false
     }
     return true
   }
 
   private readonly isEmailValid = (email: string): boolean => {
     if (/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) return true
-    throw new Error('O fomato de email deve ser "email@email.email')
+    return false
   }
 
   private readonly isAgeValid = (age: number): boolean => {
     if (age > 17 && age < 66) return true
-    throw new Error('A idade deve ser entre 18 e 65 aos')
+    return false
   }
 
   private readonly valueIsNullOrUndefined = (value: unknown): boolean => {
-    if (value === null || value === undefined) throw new Error('Nenhum valor pode ser nulo ou indefinido')
+    if (value === null || value === undefined) return true
     return false
   }
 }
