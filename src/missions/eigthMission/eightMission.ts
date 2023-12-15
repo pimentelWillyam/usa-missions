e388f8473b44a56f9df2692db24265e280d61a3a
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-15 15:01:35 -0300

    refactor: adding import and instanciating objects

--- src/missions/eigthMission/eightMission.ts
+++ src/missions/eigthMission/eightMission.ts
@@ -1,24 +1,26 @@
+import { Server } from 'http'
+
 import App from './api/App'
 import PersonController from './api/controller/PersonController'
 import PersonRouter from './api/router/PersonRouter'
 import PersonService from './api/service/PersonService'
+import PersonValidator from './api/validator/PersonValidator'
 import PersonRepository from './api/repository/PersonRepository'
 import MemoryDataSource from './data/MemoryDataSource'
 import Api from './helper/Api'
 
 import * as express from 'express'
+import ApiMiddleware from './helper/ApiMiddleware'
 
-const apiMiddleare = new ApiMiddleware()
+const apiMiddleware = new ApiMiddleware()
 
 const memoryDataSource = new MemoryDataSource()
 const personRepository = new PersonRepository(memoryDataSource)
-const personService = new PersonService(memoryDataSource)
-const personController = new PersonController(personService)
+const personService = new PersonService(personRepository)
+const personController = new PersonController(personService, new PersonValidator())
 const personRouter = new PersonRouter(personController)
 
 const api = new Api(express(), apiMiddleware, personRouter)
-const app = new App(new Api(),)
+const app = new App(api, new Server())
 
-app.listen(process.env.PORTA, () => {
-  console.log(`aplicação iniciada na porta ${process.env.PORTA}`)
-})
+app.start()
