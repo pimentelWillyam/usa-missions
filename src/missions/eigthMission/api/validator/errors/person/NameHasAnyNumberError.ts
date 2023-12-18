95bdc17dde2ecfbbf23e66097938331805af4ba0
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-17 21:27:30 -0300

    refactor: renaming custom error to known error

--- src/missions/eigthMission/api/validator/errors/person/NameHasAnyNumberError.ts
+++ src/missions/eigthMission/api/validator/errors/person/NameHasAnyNumberError.ts
@@ -1,6 +1,6 @@
-import CustomError from '../CustomError'
+import KnownError from '../KnownError'
 
-class NameHasAnyNumberError extends CustomError {
+class NameHasAnyNumberError extends KnownError {
   constructor () {
     super('Nome possui algum número', 'Não é possivel cadastrar um nome que contenha números', 400)
   }
