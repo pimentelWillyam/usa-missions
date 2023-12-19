e5cad86a9add8fe8d961c88279e8c66a5445cdad
Willyam Pimentel <willyampimenteldev@gmail.com>
2023-12-19 05:44:50 -0300

    delete: removing old normalizer to adhere to singe responsability principle

--- src/missions/eigthMission/api/service/PersonService.ts
+++ src/missions/eigthMission/api/service/PersonService.ts
@@ -1,29 +1,17 @@
 import type Person from '../entity/Person'
+import { type INameNormalizer } from '../interface/INameNormalizer'
 import type IPersonRepository from '../interface/IPersonRepository'
 import type IPersonService from '../interface/IPersonService'
 
 class PersonService implements IPersonService {
-  constructor (readonly PersonRepository: IPersonRepository) {}
+  constructor (readonly PersonRepository: IPersonRepository, readonly fullNameNormalizer: INameNormalizer) {}
 
   create (name: string, email: string, age: number): Person {
-    return this.PersonRepository.create(this.fullNameNormalizer(name), email, age)
+    return this.PersonRepository.create(this.fullNameNormalizer.normalize(name), email, age)
   }
 
   getAll (): Person[] {
     return this.PersonRepository.getAll()
-  }
-
-  private readonly fullNameNormalizer = (rawFullName: string): string => {
-    const splittedFullName = rawFullName.split(' ')
-    let i; let j; let normalizedFullname = ''
-    for (i = 0; i < splittedFullName.length; i++) {
-      if (i !== 0) normalizedFullname += ' '
-      for (j = 0; j < splittedFullName[i].length; j++) {
-        if (j === 0) normalizedFullname += splittedFullName[i][j].toLocaleUpperCase()
-        else normalizedFullname += splittedFullName[i][j].toLocaleLowerCase()
-      }
-    }
-    return normalizedFullname
   }
 }
 
